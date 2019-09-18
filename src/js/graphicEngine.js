7eb07c8e9d594927c307d34bc29dcccd667b4956
Marco <marco.modena@protonmail.com>
2019-09-18 11:15:02 -0300

    pages setup

--- src/js/graphicEngine.js
+++ src/js/graphicEngine.js
@@ -4,17 +4,17 @@ const engine = () => {
     if (screen === "menu") canvas.setCanvasHeight(400);
     switch (screen) {
         case "menu":
-            canvas.drawFullBackground("src/images/hands.png");
+            canvas.drawFullBackground("./src/images/hands.png");
             canvas.drawPill(pill);
             break;
         case "victory":
-            canvas.drawSmallBackground("src/images/flag.png");
+            canvas.drawSmallBackground("./src/images/flag.png");
             break;
         case "defeat":
-            canvas.drawSmallBackground("src/images/explosion.png");
+            canvas.drawSmallBackground("./src/images/explosion.png");
             break;
         case "map":
-            canvas.drawFullBackground("src/images/map.png");
+            canvas.drawFullBackground("./src/images/map.png");
             canvas.drawMap(board.getTerritorries());
             break;
     };
