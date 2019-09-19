90755ad25c23ce9fc3355162df4b906c52ae7f50
Marco <marco.modena@protonmail.com>
2019-09-19 18:11:38 -0300

    mvp plus music fix

--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,10 @@
 const canvas = new drawCanvas();
 const subMenu = new SubMenu();
-let song = new this.Audio("./src/songs/theme-song.mp3");
-song.loop = true;
-song.volume = 0.25;
 
 window.onload = function () {
+    let song = new this.Audio("./src/songs/theme-song.mp3");
+    song.loop = true;
+    song.volume = 0.25;
     song.play();
 
     document.getElementsByClassName("start")[0].onclick = function () {
